99feb64eab24fac21a3df1d2beeb8c8e2cdf361a
Aron Mooney <aronmooney913@gmail.com>
2024-10-23 15:54:52 +0100

    Episodes increase

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+const maxepisodes = 300;
+let currentepisodes = 15;
+
 const percentageCircle = document.querySelector('.percentage-circle');
+const increButton = document.getElementById('testbutton');
+
+increButton.addEventListener('click', () => {
+    currentepisodes = parseFloat(currentepisodes)
+    let percent = (currentepisodes/maxepisodes) * 100;
+    console.log(percent);
+    document.getElementById('narutopercent').style.setProperty('--percentage', percent);
+    currentepisodes += 1;
+});
 
 percentageCircle.addEventListener('mouseenter', () => {
     percentageCircle.classList.add('persistent-animation');
