3c5ed7ba7d13c067d433b29be3dba960c95ed3e1
Aron Mooney <aronmooney913@gmail.com>
2024-10-23 18:01:36 +0100

    Percentage animations in inner circle

--- script.js
+++ script.js
@@ -2,24 +2,43 @@ const maxepisodes = 300;
 let currentepisodes = 15;
 
 const percentageCircle = document.querySelector('.percentage-circle');
-const increButton = document.getElementById('testbutton');
+const innerCircle = document.querySelector('.innercircle');
+const text1 = document.querySelector('.text1');
+const increButtons = document.getElementsByClassName('addbutton');
+const decreButtons = document.getElementsByClassName('removebutton');
 
-increButton.addEventListener('click', () => {
-    currentepisodes += 1; // Increment the value first
-    let currentepisodes2 = parseFloat(currentepisodes)
-    let percent = (currentepisodes2/maxepisodes) * 100;
-    console.log(percent);
-    document.getElementById('narutopercent').style.setProperty('--percentage', percent);
-    const episodesContent = `${currentepisodes}/300`;
-    console.log(episodesContent); // Log the content value
-    document.getElementById('narutoinner').innerText = episodesContent;
-    
+Array.from(increButtons).forEach(button => {
+    button.addEventListener('click', () => {
+        currentepisodes += 1;
+        let currentepisodes2 = parseFloat(currentepisodes);
+        let percent = (currentepisodes2 / maxepisodes) * 100;
+        console.log(percent);
+        document.getElementById('narutopercent').style.setProperty('--percentage', percent);
+        const episodesContent = `${currentepisodes}/300`;
+        document.querySelector('.text1').innerText = episodesContent;
+        let percentage = parseInt(percent);
+        document.querySelector('.text2').innerText = `${percentage}%`;
+    });
+});
+
+Array.from(decreButtons).forEach(button => {
+    button.addEventListener('click', () => {
+        currentepisodes -= 1;
+        let currentepisodes2 = parseFloat(currentepisodes);
+        let percent = (currentepisodes2 / maxepisodes) * 100;
+        console.log(percent);
+        document.getElementById('narutopercent').style.setProperty('--percentage', percent);
+        const episodesContent = `${currentepisodes}/300`;
+        document.querySelector('.text1').innerText = episodesContent;
+    });
 });
 
 percentageCircle.addEventListener('mouseenter', () => {
     percentageCircle.classList.add('persistent-animation');
+    text1.classList.add('lowering_anim');
 });
 
 percentageCircle.addEventListener('mouseleave', () => {
     percentageCircle.classList.remove('persistent-animation');
+    text1.classList.remove('lowering_anim');
 });
