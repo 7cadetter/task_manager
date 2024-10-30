865d31acd43cb6c2787ff61e7f4c3a8221018f72
Aron Mooney <aronmooney913@gmail.com>
2024-10-30 12:19:52 +0000

    Separate counters for boxes

--- script.js
+++ script.js
@@ -1,58 +1,66 @@
-const maxepisodes = 300;
-let currentepisodes = 295;
-
 const percentageCircle = document.querySelector('.percentage-circle');
 const innerCircle = document.querySelector('.innercircle');
 const text1 = document.querySelector('.text1');
-const increButtons = document.getElementsByClassName('addbutton');
-const decreButtons = document.getElementsByClassName('removebutton');
 
-fetch('http://localhost:3000/data')
-    .then(response => response.json())
-    .then(data => {
-        const mediaContainer = document.getElementById('mediaContainer');
-        data.forEach(item => {
-            const div = document.createElement('div');
-            const title = item.title;
-            const episodes = item.episodes;
-            const watched = item.watched;
-            div.innerHTML = `${title}, ${episodes}, ${watched}`;
-            mediaContainer.appendChild(div);
+// fetch('http://localhost:3000/data')
+//     .then(response => response.json())
+//     .then(data => {
+//         const mediaContainer = document.getElementById('mediaContainer');
+//         data.forEach(item => {
+//             const div = document.createElement('div');
+//             const title = item.title;
+//             const episodes = item.episodes;
+//             const watched = item.watched;
+//             div.innerHTML = `${title}, ${episodes}, ${watched}`;
+//             mediaContainer.appendChild(div);
+//         });
+//     })
+//     .catch(error => console.error('Error fetching data:', error));
+
+document.addEventListener('DOMContentLoaded', () => {
+    const addButtons = document.querySelectorAll('.addbutton');
+    const removeButtons = document.querySelectorAll('.removebutton');
+
+    addButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const itemBox = button.closest('.itembox');
+            const currentEpisodes = parseInt(itemBox.getAttribute('data-current'));
+            const maxEpisodes = parseInt(itemBox.getAttribute('data-max'));
+
+            if (currentEpisodes < maxEpisodes) {
+                const newEpisodes = currentEpisodes + 1;
+                itemBox.setAttribute('data-current', newEpisodes);
+                const percent = (newEpisodes / maxEpisodes) * 100;
+
+                const percentCircle = itemBox.querySelector('.percentage-circle');
+                percentCircle.style.setProperty('--percentage', percent);
+
+                const innerCircle = itemBox.querySelector('.innercircle');
+                innerCircle.querySelector('.text1').innerText = `${newEpisodes}/${maxEpisodes}`;
+                innerCircle.querySelector('.text2').innerText = `${Math.round(percent)}%`;
+            }
         });
-    })
-    .catch(error => console.error('Error fetching data:', error));
-
-Array.from(increButtons).forEach(button => {
-    button.addEventListener('click', () => {
-        if (currentepisodes < maxepisodes)
-        {
-            currentepisodes += 1;
-            let currentepisodes2 = parseFloat(currentepisodes);
-            let percent = (currentepisodes2 / maxepisodes) * 100;
-            console.log(percent);
-            document.getElementById('narutopercent').style.setProperty('--percentage', percent);
-            const episodesContent = `${currentepisodes}/300`;
-            document.querySelector('.text1').innerText = episodesContent;
-            let percentage = parseInt(percent);
-            document.querySelector('.text2').innerText = `${percentage}%`;
-        }
     });
-});
 
-Array.from(decreButtons).forEach(button => {
-    button.addEventListener('click', () => {
-        if (currentepisodes > 0)
-        {
-            currentepisodes -= 1;
-            let currentepisodes2 = parseFloat(currentepisodes);
-            let percent = (currentepisodes2 / maxepisodes) * 100;
-            console.log(percent);
-            document.getElementById('narutopercent').style.setProperty('--percentage', percent);
-            const episodesContent = `${currentepisodes}/300`;
-            document.querySelector('.text1').innerText = episodesContent;
-            let percentage = parseInt(percent);
-            document.querySelector('.text2').innerText = `${percentage}%`;
-        }
+    removeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const itemBox = button.closest('.itembox');
+            const currentEpisodes = parseInt(itemBox.getAttribute('data-current'));
+            const maxEpisodes = parseInt(itemBox.getAttribute('data-max'));
+
+            if (currentEpisodes > 0) {
+                const newEpisodes = currentEpisodes - 1;
+                itemBox.setAttribute('data-current', newEpisodes);
+                const percent = (newEpisodes / maxEpisodes) * 100;
+
+                const percentCircle = itemBox.querySelector('.percentage-circle');
+                percentCircle.style.setProperty('--percentage', percent);
+
+                const innerCircle = itemBox.querySelector('.innercircle');
+                innerCircle.querySelector('.text1').innerText = `${newEpisodes}/${maxEpisodes}`;
+                innerCircle.querySelector('.text2').innerText = `${Math.round(percent)}%`;
+            }
+        });
     });
 });
 
